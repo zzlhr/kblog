903f729feda66b1edceac4a61b45b33e895e04eb
zzlhr <2388399752@qq.com>
2018-10-11 14:14:22 +0800

    clean

--- source/kblog/target/kaptStubs/compile/com/zzlhr/kblog/repository/ArticleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.zzlhr.kblog.repository;
-
-import java.lang.System;
-
-@kotlin.Metadata(mv = {1, 1, 10}, bv = {1, 0, 2}, k = 1, d1 = {"\u0000\u0012\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0002\u0010\b\n\u0000\bg\u0018\u00002\u000e\u0012\u0004\u0012\u00020\u0002\u0012\u0004\u0012\u00020\u00030\u0001\u00a8\u0006\u0004"}, d2 = {"Lcom/zzlhr/kblog/repository/ArticleRepository;", "Lorg/springframework/data/jpa/repository/JpaRepository;", "Lcom/zzlhr/kblog/entity/Article;", "", "kblog"})
-@org.springframework.stereotype.Repository()
-public abstract interface ArticleRepository extends org.springframework.data.jpa.repository.JpaRepository<com.zzlhr.kblog.entity.Article, java.lang.Integer> {
-}
